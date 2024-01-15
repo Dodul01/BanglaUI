764078b443866f538d08f6458aaf7bef2b00f787
Dodul01 <mhdodul299@gmail.com>
2024-01-15 23:07:52 +0600

    Responsive Menu Added

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -2,12 +2,15 @@ import { Link } from "react-router-dom"
 import GradientText from "../GradientText/GradientText"
 import { FaGithub } from "react-icons/fa";
 import { RiMenu4Fill } from "react-icons/ri";
+import { useState } from "react";
 
 const Nav = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
         <nav className="flex items-center lg:justify-between md:justify-between p-2 bg-blend-saturation">
-            <div className="lg:hidden md:hidden visible">
-                <RiMenu4Fill  className="text-2xl m-3"/>
+            <div className="lg:hidden md:hidden visible cursor-pointer" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                <RiMenu4Fill className="text-2xl m-3" />
             </div>
             <Link to='/' className="cursor-pointer">
                 <GradientText text={'BanglaUI'} />
@@ -18,6 +21,17 @@ const Nav = () => {
                 <Link className="cursor-pointer" to='https://github.com/Dodul01/BanglaUI'>
                     <FaGithub className="text-white text-2xl" />
                 </Link>
+            </div>
+
+            <div className={`lg:hidden md:hidden flex fixed top-[60px] ${isMenuOpen ? "right-[-15px]" : 'right-[500px]'} z-10 h-screen w-screen bg-[#08080a] transition-all`}>
+                <div className="flex flex-col gap-3 px-5">
+                    <Link to='/quickStart' className="bg-gradient-to-r bg-white transition-all hover:from-red-600 hover:to-yellow-600 inline-block text-transparent bg-clip-text font-medium">Quick Start</Link>
+                    <Link to='/components' className="bg-gradient-to-r bg-white transition-all hover:from-red-600 hover:to-yellow-600 inline-block text-transparent bg-clip-text font-medium">Components</Link>
+                    <Link className="cursor-pointer" to='https://github.com/Dodul01/BanglaUI'>
+                        <FaGithub className="text-white text-2xl" />
+                    </Link>
+                </div>
+
             </div>
         </nav>
     )
